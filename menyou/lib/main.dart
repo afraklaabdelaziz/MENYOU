d0522176bc2e2cb8a7f395eee864c41b582b75b7
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2023-03-06 10:54:14 +0100

    ajouter dependencie pour configurer firebase

--- menyou/lib/main.dart
+++ menyou/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:menyou/screens/drawer_screen.dart';
 import 'package:menyou/screens/favorie.dart';
